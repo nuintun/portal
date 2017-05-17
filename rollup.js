5a383b8f6435fd8a926279834b1f6d4463f2ce8f
nuintun <nuintun@qq.com>
2017-05-17 18:30:47 +0800

    update deps

--- rollup.js
+++ rollup.js
@@ -7,9 +7,11 @@ rollup.rollup({
   entry: 'src/portal.js',
 }).then(function(bundle) {
   let stat;
-  const map = 'portal.js.map';
-  const src = 'dist/portal.js';
-  const min = 'dist/portal.min.js';
+  const name = 'portal';
+  const filename = name + '.js';
+  const map = filename + '.map';
+  const src = 'dist/' + filename;
+  const min = 'dist/' + name + '.min.js';
 
   try {
     stat = fs.statSync('dist')
@@ -32,17 +34,20 @@ rollup.rollup({
   fs.writeFileSync(src, result.code);
   console.log(`  Build ${ src } success!`);
 
-  result = uglify.minify(result.code, {
-    fromString: true,
-    compress: { screw_ie8: false },
-    mangle: { screw_ie8: false },
-    output: { screw_ie8: false },
-    outSourceMap: map
+  var source = {};
+
+  source[filename] = result.code;
+
+  result = uglify.minify(source, {
+    compress: { ie8: true },
+    mangle: { ie8: true },
+    output: { ie8: true },
+    sourceMap: { url: map }
   });
 
   fs.writeFileSync(min, result.code);
   console.log(`  Build ${ min } success!`);
-  fs.writeFileSync(src + '.map', result.map.replace('"sources":["?"]', '"sources":["portal.js"]'));
+  fs.writeFileSync(src + '.map', result.map);
   console.log(`  Build ${ src + '.map' } success!`);
 }).catch(function(error) {
   console.error(error);
